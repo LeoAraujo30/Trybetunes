7a295f18b2bdec62733c0e7dc9c432ff0006666f
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-07-01 21:24:10 -0300

    requisito 2 feito

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { createUser } from './services/userAPI';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -9,11 +10,50 @@ import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      loading: false,
+      login: false,
+    };
+  }
+
+  inpChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  logChange = () => {
+    const TIME = 2000;
+    const { userName } = this.state;
+    this.setState({
+      loading: true,
+    }, () => createUser({ name: userName }));
+    setTimeout(() => {
+      this.setState({
+        login: true,
+      });
+    }, TIME);
+  }
+
   render() {
+    const { userName, loading, login } = this.state;
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" render={ () => <Login /> } />
+          <Route exact path="/">
+            <Login
+              userName={ userName }
+              loading={ loading }
+              login={ login }
+              inpChange={ this.inpChange }
+              logChange={ this.logChange }
+            />
+          </Route>
           <Route path="/search" render={ () => <Search /> } />
           <Route path="/album/:id" render={ () => <Album /> } />
           <Route path="/favorites" render={ () => <Favorites /> } />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
 
 class Login extends React.Component {
   render() {
+    const { userName, loading, login, inpChange, logChange } = this.props;
+    const MIN = 3;
+    if (login === true) return <Redirect to="/search" />;
+    if (loading === true) return <Loading />;
     return (
       <div data-testid="page-login">
-        login
+        <label htmlFor="name">
+          <input
+            type="text"
+            id="name"
+            name="userName"
+            data-testid="login-name-input"
+            value={ userName }
+            onChange={ inpChange }
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ userName.length < MIN }
+            onClick={ logChange }
+          >
+            Entrar
+          </button>
+        </label>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  userName: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired,
+  login: PropTypes.bool.isRequired,
+  inpChange: PropTypes.func.isRequired,
+  logChange: PropTypes.func.isRequired,
+};
 
 export default Login;
