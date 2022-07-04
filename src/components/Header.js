5beb052ea9d8f244e8a00fd156e650437b7b64a1
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-07-04 18:54:12 -0300

    requisito 4 feito

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -30,6 +31,9 @@ class Header extends React.Component {
         <p data-testid="header-user-name">
           { user.length === 0 ? <Loading /> : user }
         </p>
+        <Link data-testid="link-to-search" to="/search">Search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
       </header>
     );
   }
